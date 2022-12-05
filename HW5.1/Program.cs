ab6c33fd56ffaf5884333ef743f867bf85b61e5b
ksen29ph <119218516+ksen29ph@users.noreply.github.com>
2022-12-05 20:11:47 +0300

    Add files via upload

---
+++ HW5.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+
+if(anyNumberText.Length > 2)
+{
+    Console.WriteLine("Третья цифра вашего числа: " + anyNumberText[2]);
+}
+else
+{
+    Console.WriteLine("В вашем числе третьей цифры нет.");
+}
---
+++ HW5.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите цифру от 1 до 7 и узнаете является ли этот день выходным: ");
+
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if(dayNumber == 6 || dayNumber == 7)
+{
+    Console.WriteLine("Выходной!");
+}
+else if(dayNumber < 1 || dayNumber > 7)
+{
+    Console.WriteLine("Необходимо ввести цифру от 1 до 7. Повторите запрос.");
+}
+else Console.WriteLine("Будний день.");
